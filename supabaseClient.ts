da5118d50b40f54032dff4c063c22809ecf0f39e
MW-Limah <cdrmw16@gmail.com>
2025-03-13 17:16:58 +0000

    .env

--- supabaseClient.ts
+++ supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://jcbgfgsmuymsmwxtcnvd.supabase.co';
-const supabaseKey =
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpjYmdmZ3NtdXltc213eHRjbnZkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE4MTYwMDAsImV4cCI6MjA1NzM5MjAwMH0.vE2efao2FFgMPTtkcEmnxw8n9vai3u2Vb_bOLFb1T3M';
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+
 export const supabase = createClient(supabaseUrl, supabaseKey);
