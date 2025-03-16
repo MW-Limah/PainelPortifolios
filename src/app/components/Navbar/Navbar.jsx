9c6ef1d3129613520b0b6de5942bb376653b269e
MW-Limah <cdrmw16@gmail.com>
2025-03-16 01:43:07 +0000

    Segunda media query
    Media query na Navbar. Adicionei também um 'switch' para trocar os links por um 'FaBars', onde quando clicarmos no FaBars, os links vem na tela.

--- src/app/components/Navbar/Navbar.jsx
+++ src/app/components/Navbar/Navbar.jsx
@@ -1,30 +1,55 @@
-import styles from './Navbar.module.css';
+'use client';
+
+import { useState } from 'react';
 import Link from 'next/link';
+import { FaBars, FaTimes } from 'react-icons/fa';
 import Image from 'next/image';
+import styles from './Navbar.module.css';
 
 export default function Navbar() {
+    const [isActive, setIsActive] = useState(false);
+
+    const toggleMenu = () => {
+        setIsActive(!isActive);
+    };
+
     return (
         <nav className={styles.navbar}>
-            <Link href="/" className={styles.logo}>
-                <Image
-                    src={'/logoW.png'}
-                    width={50}
-                    height={50}
-                    alt="minha logo versão branca"
-                    className={styles.img}
-                />
-            </Link>
-            <ul className={styles.navLinks}>
-                <li>
-                    <Link href="/">Home</Link>
-                </li>
-                <li>
-                    <Link href="/projects">Projetos</Link>
-                </li>
-                <li>
-                    <Link href="/contact">Contato</Link>
-                </li>
-            </ul>
+            <div className={styles.maxWidth}>
+                {/* Logo */}
+                <Link href="/" className={styles.logo}>
+                    <Image
+                        src={'/logoW.png'}
+                        width={50}
+                        height={50}
+                        alt="minha logo versão branca"
+                        className={styles.img}
+                    />
+                </Link>
+
+                {/* Menu */}
+                <ul
+                    className={`${styles.menu} ${
+                        isActive ? styles.active : ''
+                    }`}
+                    id="menuSite"
+                >
+                    <li>
+                        <Link href="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link href="/projects">Projetos</Link>
+                    </li>
+                    <li>
+                        <Link href="/contact">Contato</Link>
+                    </li>
+                </ul>
+
+                {/* Botão Mobile */}
+                <div className={styles.menuBtn} onClick={toggleMenu}>
+                    {isActive ? <FaTimes /> : <FaBars />}
+                </div>
+            </div>
         </nav>
     );
 }
