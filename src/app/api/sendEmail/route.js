78ff8b585c8bac095384b7af45fc50e721213d3e
MW-Limah <cdrmw16@gmail.com>
2025-03-20 17:59:54 +0000

    Update route.js

--- src/app/api/sendEmail/route.js
+++ src/app/api/sendEmail/route.js
@@ -1,32 +1,28 @@
 import nodemailer from 'nodemailer';
 
-export default async function handler(req, res) {
-    if (req.method === 'POST') {
-        const { name, email, subject, message } = req.body;
+export async function POST(request) {
+    const { name, email, subject, message } = await request.json();
 
-        const transporter = nodemailer.createTransport({
-            service: 'gmail',
-            auth: {
-                user: process.env.EMAIL_USER,
-                pass: process.env.EMAIL_PASS,
-            },
-        });
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASS,
+        },
+    });
 
-        const mailOptions = {
-            from: process.env.EMAIL_USER, // Seu e-mail configurado no transporter
-            to: 'seuemail@gmail.com', // O e-mail que vai receber a mensagem
-            subject: `Contato de ${name}: ${subject}`,
-            text: `Nome: ${name}\nE-mail: ${email}\nMensagem:\n${message}`,
-        };
+    const mailOptions = {
+        from: process.env.EMAIL_USER,
+        to: process.env.EMAIL_USER,
+        subject: `Contato de ${name}: ${subject}`,
+        text: `Nome: ${name}\nE-mail: ${email}\nMensagem:\n${message}`,
+    };
 
-        try {
-            await transporter.sendMail(mailOptions);
-            res.status(200).json({ message: 'E-mail enviado com sucesso!' });
-        } catch (error) {
-            console.error('Erro ao enviar e-mail:', error);
-            res.status(500).json({ error: 'Erro ao enviar e-mail' });
-        }
-    } else {
-        res.status(405).json({ error: 'Método não permitido' });
+    try {
+        await transporter.sendMail(mailOptions);
+        return new Response(JSON.stringify({ message: 'E-mail enviado com sucesso!' }), { status: 200 });
+    } catch (error) {
+        console.error('Erro ao enviar e-mail:', error);
+        return new Response(JSON.stringify({ error: 'Erro ao enviar e-mail' }), { status: 500 });
     }
 }
