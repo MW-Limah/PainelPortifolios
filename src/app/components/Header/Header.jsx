371acf22a9455cfd47f068aa627d14f233de4b17
MW-Limah <cdrmw16@gmail.com>
2025-03-21 17:17:28 +0000

    CSS e melhorias
    Melhorei o código e o css do HEADER

--- src/app/components/Header/Header.jsx
+++ src/app/components/Header/Header.jsx
@@ -1,20 +1,16 @@
 import BtnAbout from '@/app/components/buttons/ButtonAbout/BtnAbout';
-import BtnContact from '@/app/components/buttons/ButtonContactme/BtnContact';
-import Image from 'next/image';
 import styles from './Header.module.css';
 
 const Header = () => {
     return (
         <header className={styles.header}>
-            <section className={styles.headerContentRight}>
-                <div className={styles.txtHeader}>
-                    <h1>Desenvolvimento de Software</h1>
-                    <p>Por @Williams D'Lima</p>
-                </div>
-                <div className={styles.btnHeader}>
-                    <BtnAbout />
-                </div>
-            </section>
+            <div className={styles.txtHeader}>
+                <h1>Desenvolvimento de Software</h1>
+                <p>Por @Williams D'Lima</p>
+            </div>
+            <div className={styles.btnHeader}>
+                <BtnAbout />
+            </div>
         </header>
     );
 };
